534c7444c2729aa0300f1fe0f461658d1086ae13
MariCapelo <marina.gac30@gmail.com>
2024-09-12 14:29:23 -0300

    Finished Footer

--- sisgehc/src/app/page.tsx
+++ sisgehc/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from './componentes/Footer/Footer'
 
 export default function Home() {
   return (
     <>
     <h1>Olá, está funcionando =D</h1>
     <p>são amores</p>
+    <Footer></Footer>
     </>
   );
 }

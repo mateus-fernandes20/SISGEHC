9d348d1046868e48760a87567e8200d31db05205
MariCapelo <marina.gac30@gmail.com>
2024-09-15 15:41:08 -0300

    Event Cards in process

--- sisgehc/src/app/components/Card/Card.tsx
+++ sisgehc/src/app/components/Card/Card.tsx
@@ -1,32 +1,47 @@
 import './Card.css'
 import Image from 'next/image';
 import Button from '@/app/components/Button/Button';
+import { Fragment } from 'react';
 
-const Card = () => {
+async function Card() {
+    const response = await fetch('http://127.0.0.1:8000/eventos/')
+    const data = await response.json() 
+
+    console.log(data)
+
+    const responseprof = await fetch('http://127.0.0.1:8000/professor/')   
+    const dataprof = await responseprof.json()    
+
+    console.log(dataprof)
 
     return(
-        <div className='card__container'>
-            <div className='card__titulo'>
-                    <div className="card__image">
-                        
-                    </div>
-                    <p>TÍTULO</p>
-            </div>
+        <div>
+                {data.map((item: any) =>
+                <Fragment key={item.id}>
+                        <div className='card__container'>
+                                <div className='card__titulo'>
+                                        <div className="card__image">
+                                                <Image src={item.imagem} alt='Imagem evento' width={375} height={190}></Image>
+                                        </div>
+                                        <p>{item.nome}</p>
+                                </div>
 
-            <div className='car__informacoes'>
-                    <div>
-                            <p>Informação data </p>
-                            <p>Informação Local</p>
-                            <p>Informação hora</p>
-                    </div>
-                    <div>
-                            <p>Realização:</p>
-                            <p>Quem está realizando o evento</p>
-                    </div>
-                   <Button color='.btn-verde' text='Se inscrever'/>
-            </div>
+                                <div className='card__informacoes'>
+                                        <div>
+                                                <p>De {item.dataInicio} até {item.dataFim}</p>
+                                                <p>{item.professor === 2 ? 'Paulo Henrique' : 'Mateus Paixão'}</p>
+                                                <p>{item.horaInicio} - {item.horaFim}</p>
+                                        </div>
+                                        <div>
+                                                <p>Realização:</p>
+                                                <p>{item.descricao}</p>
+                                        </div>
+                                        <Button color='.btn-verde' text='Se inscrever'/>
+                                </div>
+                        </div>
+                </Fragment>)}  
         </div>
     );
 }
 
-export default Card;
+export default Card
